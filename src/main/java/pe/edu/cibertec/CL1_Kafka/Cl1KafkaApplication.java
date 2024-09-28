b6007da5b7d093ab42916888ba70df73fe822c23
Ryuunotora <144704234+Ryuunotora@users.noreply.github.com>
2024-09-27 21:30:01 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/CL1_Kafka/Cl1KafkaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_Kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1KafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1KafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/CL1_Kafka/Cl1KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_Kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
